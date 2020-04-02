13de0d984ef0a282674d9b6e438d4148392897f0
caomin <748833541@qq.com>
2020-04-02 10:37:10 +0800

    第三章 - 绘制三角形

--- src/utils/menus.ts
+++ src/utils/menus.ts
@@ -13,7 +13,13 @@ export const menus: IMenu[] = [
       { path: '/chapter2/coloredPoint', title: '鼠标画点并改变颜色' },
     ],
   },
-  { path: 'chapter3', title: '绘制和变换三角形', children: [
+  {
+    path: 'chapter3',
+    title: '绘制和变换三角形',
+    children: [
       { path: '/chapter3/multiPoints', title: '绘制多个点' },
-    ] },
+      { path: '/chapter3/helloTriangle', title: '绘制三角形' },
+      { path: '/chapter3/translatedTriangle', title: '变换三角形' },
+    ],
+  },
 ];
